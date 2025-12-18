c4ea23d4eff1abd445d54e79c7f81f357276d297
naveen <naveenreddy34560@gmail.com>
2025-12-18 08:38:07 +0530

    new leet code

---
+++ leet code/moves.java
@@ -0,0 +1,16 @@
+public class moves {
+    
+        import java.util.Arrays;
+        int count=0;
+
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0){
+                int temp =nums[i];
+                nums[i]=nums[count];
+                nums[count]=temp;
+                count++;
+            }
+        }
+        System.out.println(Arrays.toString(nums));
+
+    }
